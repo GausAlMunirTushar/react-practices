9039f2692f6b817b1a04507f0c98a8fb99c3b4e4
Gaus Al Munir Tushar <tushar01910533370@gmail.com>
2022-10-26 17:58:56 +0600

    react practice files and folder added

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../components/Home/Header';
+const Home = () => {
+    return (
+        <div>
+            <Header title="welcome to Home Page" para="this is paragraph"></Header>
+
+        </div>
+    );
+};
+
+export default Home;
